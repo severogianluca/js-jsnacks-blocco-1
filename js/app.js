4a19e76ac09e4a8e977cd61a57d5f337ba46f9bf
severogianluca <gianlucasevero300@gmail.com>
2025-02-27 12:36:01 +0100

    fix

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-let numero1 = Number (prompt('Inserisci il primo numero'));
-let numero2 = Number (prompt('Inserisci il secondo numero'));
+// SNACK 1
+// creo i prompt dove l'utente inserirà i numeri
+let numero1 = parseFloat (prompt('Inserisci il primo numero'));
+let numero2 = parseFloat (prompt('Inserisci il secondo numero'));
 
-
+// creo delle condizioni in modo tale che uscirà il numero piu grande inserito dall'utente
 if(numero1 < numero2){
     console.log('Il numero piu alto è il secondo: ' + `${numero2}`);
 }else if(numero1 > numero2){
@@ -9,3 +11,8 @@ if(numero1 < numero2){
 }else{
     console.error('i numeri sono uguali')
 }
+
+
+// SNACK 2
+let firstString = prompt('Inserisci la prima frase');
+let secondString = prompt('Inserisci la seconda frase');
