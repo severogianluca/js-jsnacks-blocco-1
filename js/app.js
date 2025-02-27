72ea03170d0125a27a5070dd8abc58d766f7ef8e
severogianluca <gianlucasevero300@gmail.com>
2025-02-27 12:45:58 +0100

    fix: snack 2

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 let numero1 = parseFloat (prompt('Inserisci il primo numero'));
 let numero2 = parseFloat (prompt('Inserisci il secondo numero'));
 
-// creo delle condizioni in modo tale che uscirà il numero piu grande inserito dall'utente
 if(numero1 < numero2){
     console.log('Il numero piu alto è il secondo: ' + `${numero2}`);
 }else if(numero1 > numero2){
@@ -17,12 +16,13 @@ if(numero1 < numero2){
 let firstString = prompt('Inserisci la prima frase');
 let secondString = prompt('Inserisci la seconda frase');
 
-if(firstString.length < secondString.length ){
-    console.log(`${secondString}`);
+if(firstString.length > secondString.length ){
     console.log(`${firstString}`);
-}else if(firstString.length > secondString.length ){
+    console.log(`${secondString}`);
+    
+}else if(firstString.length < secondString.length ){
     console.log(`${secondString}`);
     console.log(`${firstString}`);
 }else{
-    console.errore('le stringhe hanno la stessa lunghezza')
+    console.error('le stringhe hanno la stessa lunghezza')
 }
